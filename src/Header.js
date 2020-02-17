99b7737aeb265f58aef237a0352ea466222f001e
Gautam <gramasu000@citymail.cuny.edu>
2020-02-17 11:20:10 -0500

    Included icons from erikflowers/weather-icons in the header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
-import "./Header.css"
+import "./Header.css";
+import Icon from "./icons/Icon.js";
 
 class Header extends Component {
 
     render() {
         return (
             <nav class="Header">
+                <Icon />
                 <h1>{this.props.name}</h1>
             </nav>
         );
---
+++ src/icons/Icon.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./Icon.css";
+import SunnyIcon from "./SunnyIcon.js";
+import OpenWeatherIcon from "./OpenWeatherIcon.js"
+
+class Icon extends Component {
+
+    render() {
+        return (<OpenWeatherIcon />)
+    }
+}
+
+export default Icon;
