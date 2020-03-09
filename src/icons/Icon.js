a98fc1757db7795e760213aa339d08eb7daeb322
Gautam <gramasu000@citymail.cuny.edu>
2020-03-09 16:42:18 -0400

    Added night-clear icon

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "./Icon.css";
 import SunnyIcon from "./SunnyIcon.js";
+import NightClearIcon from "./NightClearIcon.js";
 import OpenWeatherIcon from "./OpenWeatherIcon.js"
 
 class Icon extends Component {
@@ -8,6 +9,8 @@ class Icon extends Component {
     render() {
         if (this.props.name === "01d") {
             return (<SunnyIcon />); 
+        } else if (this.props.name === "01n") {
+            return (<NightClearIcon />);
         } else {
             return (<OpenWeatherIcon />);
         }
