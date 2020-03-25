35d0ab01f8a763d6aa49aeb54da63905e66330f1
Gautam <gramasu000@citymail.cuny.edu>
2020-03-25 18:24:08 -0400

    Commented and Linted src/appid.js and src/TextCard.js

--- src/TextCard.js
+++ src/TextCard.js
@@ -1,23 +1,47 @@
-import React, {Component} from "react";
+/** @module TextCard
+ * Exports TextCard Component
+ *
+ * @author [Gautam Ramasubramanian](https://github.com/gramasu000)
+ */
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-class TextCard extends Component {
+/** Represents a plain or header text in a Card div */
+export default class TextCard extends Component {
+  static propTypes = {
+    /** weight can be "h2" or "p"
+     *  If "h2", text is shown using <h2> tag
+     *  Else, text is shown using the <p> tag
+     */
+    weight: PropTypes.string,
+    /** content is an object with property "text"
+     * {
+     *    text: "Text to be displayed in TextCard"
+     * }
+     */
+    content: PropTypes.object,
+  }
 
-    render() {
-        if (this.props.weight === "h2") {
-            return (
-                <div className="Card">
-                    <h2>{this.props.content.text}</h2>
-                </div>
-            );
-        } else {
-            return (
-                <div className="Card">
-                    <p>{this.props.content.text}</p>
-                </div>
-            );
-        }
+  /**
+   * @method render
+   * @return {React.Element}
+   */
+  render() {
+    /* If weight is "h2", return header2 text inside Card div
+     * Otherwise, return p text inside Card div
+     */
+    if (this.props.weight === 'h2') {
+      return (
+        <div className="Card">
+          <h2>{this.props.content.text}</h2>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Card">
+          <p>{this.props.content.text}</p>
+        </div>
+      );
     }
-
+  }
 }
-
-export default TextCard;
--- src/appid.js
+++ src/appid.js
@@ -1,4 +1,20 @@
+/* This module has the API key for OpenWeatherMap
+ * To get an API key, get an account on openweathermap.org
+ * for a free or paid subscription, go to API keys and create a key
+ *
+ * IMPORTANT: Make sure changes to this file do NOT get pushed to github
+ * if you don't want your api key to be seen by the world in github.com
+ *
+ * To do that, use the following command
+ *
+ *    git --update-index --assume-unchanged src/appid.js
+ *
+ * This will make git blind to whatever changes you make to this file
+ * For some reason if you want to undo this, do
+ * git --update-index --no-assume-unchanged src/appid.js
+ */
+
 // Put OpenWeather API key here
-let appid = "";
+const appid = '';
 
 export default appid;
